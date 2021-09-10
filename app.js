a77bd6aebb29faf00b535c6277ca292616ba1c46
aryanthesaini <68115113+aryanthesaini@users.noreply.github.com>
2021-09-10 12:32:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const jokes = document.querySelector('#jokess');
+
+const dadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } };
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return res.data.joke;
+    }
+    catch {
+        return "NO JOKES AVAILABE :(";
+    }
+
+
+}
+
+const bt = document.querySelector('button');
+
+const addNewJoke = async () => {
+
+    const jokeText = await dadJoke();
+    const newLI = document.createElement('LI');
+    newLI.className = "list-group-item list-group-item-danger";
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+
+bt.addEventListener('click', addNewJoke);
